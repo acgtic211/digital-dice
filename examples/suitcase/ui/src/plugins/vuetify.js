01f22328fd93ec0b91cce4e3154f53d103a1da38
rutzrs <rrs999@inlumine.ual.es>
2024-12-10 10:54:59 +0100

    Optimizando Dockerfiles

---
+++ examples/suitcase/ui/src/plugins/vuetify.js
@@ -0,0 +1,9 @@
+// src/plugins/vuetify.js
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+
+export default createVuetify({
+  theme: {
+    defaultTheme: 'light', // Puedes cambiar el tema si lo prefieres
+  },
+});
