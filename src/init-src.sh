a6adff2fb8c9914181284aaf907c0a207de74ad6
rutzrs <rrs999@inlumine.ual.es>
2024-12-17 09:35:44 +0100

    Kubernetes en src (reflection no funciona, el resto si)

---
+++ src/init-src.sh
@@ -0,0 +1,27 @@
+# Buildeamos las imagenes de los contenedores
+echo "##### BUILDEANDO CONTROLLER #####"
+cd controller
+docker build -t src-controller .
+
+kubectl create secret generic tls-src \
+  --from-file=privkey.pem=privkey.pem \
+  --from-file=fullchain.pem=fullchain.pem \
+  --from-file=server.crt=server.crt \
+  --from-file=server.key=server.key
+  
+echo "##### BUILDEANDO DATAHANDLER #####"
+cd ../datahandler
+docker build -t src-dh .
+echo "##### BUILDEANDO EVENTHANDLER #####"
+cd ../eventhandler
+docker build -t src-eh .
+echo "##### BUILDEANDO REFLECTION #####"
+cd ../reflection
+docker build -t src-reflection .
+
+# Aplicamos el YAML para iniciar los contenedores
+echo "##### INICIANDO YAML #####"
+
+cd ..
+
+kubectl apply -f src.yaml 
