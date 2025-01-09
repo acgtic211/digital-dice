84e580201114099b84528780e59696fce211d45d
rutzrs <rrs999@inlumine.ual.es>
2025-01-09 13:10:16 +0100

    Encapsulando rutas del datahandler en archivo rutas aparte

--- src/datahandler/src/index.js
+++ src/datahandler/src/index.js
@@ -10,13 +10,97 @@ const metricsMiddleware = promBundle({includeMethod: true});
 
 const spdy = require("spdy");
 
-var td = require("./td/td");
+const routes = require("./routes");
+
+try {
+const data = fs.readFileSync(jsonFilePath, 'utf8');
+var td = JSON.parse(data);
+} catch (err) {
+console.error('Error reading or parsing JSON file:', err);
+}
 var td_schema = require("./td/td_schema");
 
-dotenv.config();
+// Validate TD.
 
-// Connect to the mongoDB
-const db = require('./config');
+var ajv = new Ajv();
+var valid = ajv.validate(td_schema, td);
+if (!valid) {
+  console.log(ajv.errors);
+  console.log("La Thing no cumple el equema de la Thing Description");
+} else {
+  console.log("TD validada");
+
+  td.base = process.env.TD_O_URL;
+
+  td.securityDefinitions = {
+    basic_sc: {
+      scheme: "basic",
+      in: "header",
+     }
+  };
+
+  td.security = "basic_sc";
+
+  if (td.properties) {
+    for (var prop in td.properties) {
+      if (td.properties.hasOwnProperty(prop)) {
+        td.properties[prop].forms = [
+          {
+            href: `${td.base}/td.id/properties/${prop}`,
+            contentType: "application/json",
+            op: ["readproperty", "writeproperty"],
+          },
+          {
+            href: `${td.base}/td.id/properties/${prop}/sse`,
+            contentType: "application/json",
+            subprotocol: "sse",
+            op: ["readproperty"],
+          },
+        ];
+      }
+    }
+  }
+
+  if (td.actions) {
+    for (var action in td.actions) {
+      if (td.actions.hasOwnProperty(action)) {
+        td.actions[action].forms = [
+          {
+            href: `${td.base}/td.id/actions/${action}`,
+            contentType: "application/json",
+            op: ["invokeaction"],
+          },
+        ];
+      }
+    }
+  }
+
+  if (td.events) {
+    for (var event in td.events) {
+      if (td.events.hasOwnProperty(event)) {
+        td.events[event].forms = [
+          {
+            href: `${td.base}/td.id/events/${event}`,
+            contentType: "application/json",
+            op: ["subscribeevent"],
+          },
+          {
+            href: `${td.base}/td.id/events/${event}/sse`,
+            contentType: "application/json",
+            subprotocol: "sse",
+            op: ["subscribeevent"],
+          },
+        ];
+      }
+    }
+  }
+
+  console.log("TD virtual creado:", td);
+
+  process.env.TD_VIRTUAL = JSON.stringify(td);
+}
+
+dotenv.config();
 
 // Creates Web Server
 const app = express();
@@ -36,129 +120,9 @@ app.use(metricsMiddleware);
 app.use(morgan('common'));
 
 
-var thingInteractionSchema = require('./models')
-
-var ThingInteraction = db.model('ThingInteraction', thingInteractionSchema);
-
-var td = require("./td/td");
-//var td_schema = require("./td/td_schema");
-
-app.get('/' + td.id + '/status/sse', async (req, res) => {
-    
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-
-    const headers = {
-        'Content-Type': 'text/event-stream',
-        'Connection': 'keep-alive',
-        'Cache-Control': 'no-cache'
-    };
-    res.writeHead(200, headers);
-    console.log("HELLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
-
-    ThingInteraction.watch([{$match: {$and: [  { "fullDocument.origen": "physicalDevice"}, {"fullDocument.device": "acg:lab:suitcase-dd" }]}}]).on('change', change => {
-        var response = new ThingInteraction(change.fullDocument);
-        res.write(`data: ${JSON.stringify(response)}\n\n`)
-    });
-
-    req.on('close', () => {
-        console.log(`Connection closed`);
-    });
-
-})
-
-app.get('/' + td.id + '/property/:propertyName/sse', async (req, res) => {
-    
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-
-    const headers = {
-        'Content-Type': 'text/event-stream',
-        'Connection': 'keep-alive',
-        'Cache-Control': 'no-cache'
-    };
-    res.writeHead(200, headers);
-
-    ThingInteraction.watch([{$match: {$and: [  { "fullDocument.origen": "physicalDevice"}, {"fullDocument.interaction": "property."+req.params.propertyName }]}}]).on('change', change => {
-        var response = new ThingInteraction(change.fullDocument);
-        res.write(`data: ${JSON.stringify(response.data)}\n\n`)
-    }
-    );
-
-    req.on('close', () => {
-        console.log(`Connection closed`);
-    });
-
-})
-
-app.get('/' + td.id + '/property/:propertyName', async (req, res) => {
-
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-
-    ThingInteraction.findOne({ interaction: "property."+req.params.propertyName, origen: "physicalDevice" }, {}, { sort: { 'createdAt': -1 } }, function (err, data) {
-        var response = new ThingInteraction(data);
-        res.send(response.data)
-    });
-
-})
-
-app.post('/' + td.id + '/property/:propertyName', async (req, res) => {
-
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-
-    var interactionValue = new ThingInteraction({
-        device: "acg:lab:suitcase-dd",
-        origen: "user",
-        interaction: "property." + req.params.propertyName,
-        data: req.body
-    })
-
-    interactionValue.save(function (err, doc) {
-        if (err) res.status(400).send(err);
-        else {;
-            res.send(doc.data);
-        }
-    })
-
-})
-
-app.post('/' + td.id + '/action/:actionName', async (req, res) => {
-
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-
-    var interactionValue = new ThingInteraction({
-        device: "acg:lab:suitcase-dd",
-        origen: "user",
-        interaction: "action." + req.params.actionName,
-        data: req.body
-    })
-
-    interactionValue.save(function (err, doc) {
-        if (err) res.status(400).send(err);
-        else res.send(doc.data);
-    })
 
 
-})
-
-app.post('/' + td.id + '/event/:eventName', async (req, res) => {
-
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-
-    var interactionValue = new ThingInteraction({
-        device: "acg:lab:suitcase-dd",
-        origen: "eventHandler",
-        interaction: "event." + req.params.eventName,
-        data: req.body
-    })
-
-    interactionValue.save(function (err, doc) {
-        if (err) res.status(400).send(err);
-        else res.send(doc.data);
-    })
-
-
-})
-
-
+app.use("/", routes);
 
 // Initializates the Webserver
 spdy.createServer(
