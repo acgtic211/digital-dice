354c5c8f65bc4b1ee648a61cdd1280143a9803fb
rutzrs <rrs999@inlumine.ual.es>
2025-01-14 09:53:04 +0100

    Scripts para borrado de Kubernetes tanto de src como de mongo

---
+++ src/delete-mongo.sh
@@ -0,0 +1,7 @@
+kubectl delete -f mongo-v7.yaml
+
+kubectl delete pvc mongo-persistent-storage-mongo-0
+
+kubectl delete pvc mongo-persistent-storage-mongo-1
+
+kubectl delete pvc mongo-persistent-storage-mongo-2
---
+++ src/delete-src.sh
@@ -0,0 +1,5 @@
+kubectl delete -f src.yaml
+
+kubectl delete configmap tls-src
+
+kubectl delete configmap td-config
