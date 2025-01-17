e6e9a7a08649787626a5e2f77e7d92c888b670f3
rutzrs <rrs999@inlumine.ual.es>
2025-01-17 13:30:13 +0100

    Recuerda datahandler cambiar

---
+++ examples/my-lamp-example/init-src.sh
@@ -0,0 +1,15 @@
+kubectl create configmap td-config --from-file=td.json
+
+cd ./certs
+
+kubectl create secret generic tls-src \
+  --from-file=privkey.pem=privkey.pem \
+  --from-file=fullchain.pem=fullchain.pem \
+  --from-file=server.crt=server.crt \
+  --from-file=server.key=server.key
+
+echo "##### INICIANDO YAML #####"
+
+cd ..
+
+kubectl apply -f src.yaml 
