52df3cafd405cdce420f0660be4939853fcb6e53
Manel Mena <manel.mena@ual.es>
2025-05-30 12:00:45 +0200

    moving files

--- trash/v/models.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var thingInteractionSchema = new Schema({ origen: String}, { strict: false, timestamps: true });
-
-mongoose.model('ThingInteraction', thingInteractionSchema);
-
-module.exports = thingInteractionSchema
