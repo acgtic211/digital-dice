d61db843e4549fea9aa00956734b5128f9077f09
Manel Mena <manel.mena@ual.es>
2025-05-30 09:55:19 +0200

    ordering files in repo

--- src/delete-db.sh
+++
@@ -1,7 +0,0 @@
-kubectl delete -f mongo-v7.yaml
-
-kubectl delete pvc mongo-persistent-storage-mongo-0
-
-kubectl delete pvc mongo-persistent-storage-mongo-1
-
-kubectl delete pvc mongo-persistent-storage-mongo-2
